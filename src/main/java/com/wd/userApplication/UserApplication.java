e776cdabf95853335aaf7ff6bce199ffb80ad775
Neha Nutan Bara (Digital) <NE20090804@wipro.com>
2020-01-10 11:17:11 +0530

    user added

---
+++ src/main/java/com/wd/userApplication/UserApplication.java
@@ -0,0 +1,15 @@
+package com.wd.userApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserApplication.class, args);
+		System.out.println("Hello Neha");
+	}
+
+}
